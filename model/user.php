0b975818f5c306893026f1a2322dab58f010cd46
vincentve42 <vincentve4@2@gmail.com>
2025-08-14 21:51:17 +0700

    Basic File

---
+++ model/user.php
@@ -0,0 +1,18 @@
+<?php 
+
+class User {
+    public function Register(User $user)
+    {
+        
+    }
+    public function Login(User $user)
+    {
+
+    }
+    public function Save(User $user)
+    {
+
+    }
+}
+
+?>
